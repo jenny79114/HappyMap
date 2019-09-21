d1b3002e4f2aeaf2dfaa754d20c5c7f253db2516
Jenny Lin <jenny79114@hotmail.com>
2019-09-21 16:27:44 +0800

    0921

---
+++ HappyMap/DetalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalViewController.swift
+//  HappyMap
+//
+//  Created by Jenny on 2019/9/21.
+//  Copyright © 2019 Jenny. All rights reserved.
+//
+
+import UIKit
+
+class DetalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
